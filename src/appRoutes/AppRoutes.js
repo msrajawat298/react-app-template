fb0213095ddfb18463797460ba78142697262209
mayank.kushwah <mayank.kushwah@eaglevew.com>
2023-08-27 17:30:59 +0530

    enhancement: implemente of react router

---
+++ src/appRoutes/AppRoutes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../component/home/Home';
+import About from '../component/about/About';
+import Faq from '../component/faq/Faq';
+import Contact from '../component/contact/Contact';
+import PageNotFound from '../component/pagenotfound/PageNotFound';
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/faq" element={<Faq />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="*" element={<PageNotFound />} />
+  </Routes>
+);
+
+export default AppRoutes;
